ca26168e57e9f099af93a271495230f9b6a5d7a0
Vellinga <s.vellinga@cargofit.nl>
2021-01-21 21:03:30 +0100

    Make app run with python3 comment

--- app.py
+++ app.py
@@ -515,8 +515,8 @@ if __name__ == '__main__':
     app.run()
 
 # Or specify port manually:
-'''
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
-'''
+  app.run(debug=True)
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
+
