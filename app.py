700a285e349a214f19c6aa80acc829341f137bb3
Vellinga <s.vellinga@cargofit.nl>
2021-02-08 14:44:44 +0100

    Delete function for Venues

--- app.py
+++ app.py
@@ -311,12 +311,25 @@ def create_venue_submission():
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
 def delete_venue(venue_id):
-  # TODO: Complete this endpoint for taking a venue_id, and using
-  # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
+    # TODO: Complete this endpoint for taking a venue_id, and using
+    # SQLAlchemy ORM to delete a record. Handle cases where the session commit could fail.
+    Venue.query.filter_by(id=venue_id).delete()
+    try:
+        db.session.commit()
+        flash("The venue with id %r has been deleted" % venue_id)
+    except:
+        db.session.rollback()
+        flash("The venue with id %r was not deleted" % venue_id)
+    finally:
+        db.session.close()
+
+    return redirect(url_for('venues'))
+  
+  #return redirect(url_for('index'))
+  
 
   # BONUS CHALLENGE: Implement a button to delete a Venue on a Venue Page, have it so that
   # clicking that button delete it from the db then redirect the user to the homepage
-  return None
 
 #  Artists
 #  ----------------------------------------------------------------
